c0c94b52606ad3d208ca55129dac53860bdf376c
YUMYUM\유미 <yummming519@gmail.com>
2019-11-05 00:00:37 +0900

    와!

---
+++ Study/Assets/Scenes/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("와!!! 시작이다!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
